9204c828e28b15294014078212f8b5281927c623
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2022-05-27 10:06:01 -0300

    Side menu UI and Simple dashboard

---
+++ challenges/flutter/test_challenge/lib/app/modules/dashboard/dashboard_store.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_triple/flutter_triple.dart';
+
+class DashboardStore extends NotifierStore<Exception, int> {
+
+  DashboardStore() : super(0);
+
+}
---
+++ challenges/flutter/test_challenge/test/app/modules/dashboard/dashboard_store_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:test_challenge/app/modules/dashboard/dashboard_store.dart';
+ 
+void main() {
+  late DashboardStore store;
+
+  setUpAll(() {
+    store = DashboardStore();
+  });
+
+  test('increment count', () async {
+    expect(store.state, equals(0));
+    store.update(store.state + 1);
+    expect(store.state, equals(1));
+  });
+}
